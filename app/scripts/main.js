ac3bbb919e335e4cc6fe9efabb9823915e6cb623
mcorcuera <mik.corcuera@gmail.com>
2015-12-08 22:51:15 +0100

    App basic structure using Polymer elements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+(function() {
+    console.log('Initializing map');
+    var panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('map'),
+      {
+        addressControl: false,
+        zoomControl: false,
+        position: {lat: 37.869260, lng: -122.254811},
+        pov: {heading: 165, pitch: 0},
+        zoom: 1
+      });
+})();
