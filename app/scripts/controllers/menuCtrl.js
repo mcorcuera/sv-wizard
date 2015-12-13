e1ab96554b8bd513bfa134163a5c98ec4d35d5ad
mcorcuera <mik.corcuera@gmail.com>
2015-12-13 23:41:52 +0100

    GH-4 Add settings input for authentication

--- app/scripts/controllers/menuCtrl.js
+++ app/scripts/controllers/menuCtrl.js
@@ -1,6 +1,6 @@
 var svWizardApp = angular.module('svWizardApp');
 
-svWizardApp.controller( 'SettingsCtrl', ['$scope', '$settings',
+svWizardApp.controller( 'MenuCtrl', ['$scope', '$settings',
   function($scope, Settings) {
 
 }]);
---
+++ app/streetViewRequestCtrl.js
@@ -0,0 +1,45 @@
+var svWizardApp = angular.module('svWizardApp');
+
+
+svWizardApp.controller( 'StreetViewRequestCtrl', ['$scope', 'ngDialog', 'M',
+  'Generator', 'Settings', function($scope, ngDialog, M, Generator, Settings) {
+  var currentRequest = {
+    name: 'La Giralda',
+    location: {lat: 37.38629, lng: -5.99195},
+    size: {
+      width: 640,
+      height: 640
+    },
+    heading: 241.5,
+    fov: 57,
+    pitch: 27,
+    authenticationMode: M.AuthenticationMode.NONE
+  };
+
+  $scope.getRatio = function() {
+    var width = $scope.request.size.width;
+    var height = $scope.request.size.height;
+
+    if( width == 0 || height == 0) {
+      return 1;
+    }else{
+      return width/height;
+    }
+  }
+
+  $scope.AuthenticationMode = M.AuthenticationMode;
+
+  $scope.generate = function() {
+    var url = Generator.generate($scope.request, Settings.getSettings());
+    ngDialog.open({
+      template: 'templates/generated.html',
+      className: 'ngdialog-theme-default ngdialog-theme-custom',
+      controller: 'GeneratedDialogCtrl',
+      data: {
+        url: url
+      }
+    });
+  }
+  
+  $scope.request = currentRequest;
+}]);
